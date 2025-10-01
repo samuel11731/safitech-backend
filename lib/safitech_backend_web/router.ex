efebef6f940ad824b2415edc4a57085bd37b2888
Samuel11731 <mutindasam39@gmail.com>
2025-10-01 15:12:40 +0300

    fix: use app layout in router

--- lib/safitech_backend_web/router.ex
+++ lib/safitech_backend_web/router.ex
@@ -8,7 +8,7 @@ defmodule SafitechBackendWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, html: {SafitechBackendWeb.Layouts, :false}
+    plug :put_root_layout, html: {SafitechBackendWeb.Layouts, :app}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :fetch_current_scope_for_user

57f976e12bf4907e646c350bdebab7ca16f774d5
Samuel11731 <mutindasam39@gmail.com>
2025-10-01 12:33:26 +0300

    fix: remove broken migration that references non-existent table

--- priv/repo/migrations/20250930123312_add_is_read_to_messages.exs
+++
@@ -1,9 +0,0 @@
-defmodule SafitechBackend.Repo.Migrations.AddIsReadToMessages do
-  use Ecto.Migration
-
-  def change do
-    alter table(:messages) do
-      add :is_read, :boolean, default: false, null: false
-    end
-  end
-end
